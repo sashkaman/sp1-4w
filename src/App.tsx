a1d3bf18d987ab6ef016140e52dc3628090a3fed
Alexander <rzn.russia4912@gmail.com>
2026-01-12 22:58:30 +0300

    check commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
-import {useState} from 'react'
-import {v1} from 'uuid'
-import {TodolistItem} from './TodolistItem'
+import { useState } from 'react'
+import { v1 } from 'uuid'
+import { TodolistItem } from './TodolistItem'
 
 export type Task = {
   id: string
@@ -17,6 +17,7 @@ export const App = () => {
   const [tasks, setTasks] = useState<Task[]>([
     { id: v1(), title: 'HTML&CSS', isDone: true },
     { id: v1(), title: 'JS', isDone: true },
+    { id: v1(), title: 'ReactJS', isDone: false },
     { id: v1(), title: 'ReactJS', isDone: false },
   ])
 
@@ -40,18 +41,18 @@ export const App = () => {
   }
 
   const createTask = (title: string) => {
-    const newTask = {id: v1(), title, isDone: false}
+    const newTask = { id: v1(), title, isDone: false }
     const newTasks = [newTask, ...tasks]
     setTasks(newTasks)
   }
 
   return (
-      <div className="app">
-        <TodolistItem title="What to learn"
-                      tasks={filteredTasks}
-                      deleteTask={deleteTask}
-                      changeFilter={changeFilter}
-                      createTask={createTask}/>
-      </div>
+    <div className="app">
+      <TodolistItem title="What to learn"
+        tasks={filteredTasks}
+        deleteTask={deleteTask}
+        changeFilter={changeFilter}
+        createTask={createTask} />
+    </div>
   )
 }
